27f49d538718cee5444b8ae6efdeb5933c659f05
Teddy Smoker <pbadarau@gmail.com>
2017-07-08 14:21:31 +0000

    Refactor show action to use set_competition before_action

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,9 +1,16 @@
 class CompetitionsController < ApplicationController
+  before_action :set_competition, only: [:show]
+  
   def index
     @competitions = Competition.all
   end
   
   def show
-    @competition = Competition.find(params[:id])
   end
+  
+  private
+  
+    def set_competition
+      @competition = Competition.find(params[:id])
+    end
 end
