9def76032d4aac6d6c4ee3f3697a47276d59bb09
Teddy Smoker <pbadarau@gmail.com>
2017-07-10 12:32:57 +0000

    Add pick_winner action

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,5 +1,5 @@
 class CompetitionsController < ApplicationController
-  before_action :set_competition, only: [:show, :edit, :update]
+  before_action :set_competition, only: [:show, :edit, :update, :pick_winner]
   
   def index
     @competitions = Competition.all
@@ -30,6 +30,22 @@ class CompetitionsController < ApplicationController
       redirect_to @competition, notice: 'Your competition was successfully updated!'
     else
       render :edit, notice: 'There was an error processing your request!'
+    end
+  end
+  
+  # It might need a refactor
+  def pick_winner
+    if @competition.winner
+      redirect_to competition_path(@competition)
+    else
+      if not @competition.competitors.count == 0
+        picked_winner = @competition.competitors.order("RANDOM()").first.phone_number
+        @competition.winner = picked_winner
+        @competition.save
+        redirect_to competition_path(@competition), notice: 'Successfully Picked a Winner!'
+      else
+        redirect_to competition_path(@competition), notice: 'There are no competitors yet!' 
+      end
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'competitions#index'
   resources :competitions
+  post '/competitions/:id', to: 'competitions#pick_winner', as: 'pick_winner'
 end
