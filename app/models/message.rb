ecba4b322802b5252e3870f0d207059005dbffe5
Teddy Smoker <pbadarau@gmail.com>
2017-07-12 15:33:38 +0000

    Add scopes :for_number, :by_date and :recent_by_number

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+  scope :for_number,        -> (number) { where(number: number).by_date }
+  scope :by_date,           -> { order(created_at: :desc) }
+  scope :recent_by_number,  -> { group(:number).having('created_at = MAX(created_at)') }
 end
