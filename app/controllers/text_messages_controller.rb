615d9d02d45b1bb7d894c8a39255d41f37c1ac02
Teddy Smoker <pbadarau@gmail.com>
2017-07-12 19:54:12 +0000

    Add a method to create sms messages

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -5,11 +5,30 @@ class TextMessagesController < ApplicationController
   
   # If webhooks are set up as GET requests
   def index
-    render json: { state: 200 }
+    create_message(params)
   end
   
   # If webhooks are set up as POST requests
   def create
-    render json: { state: 200 }
+    create_message(params)
   end
+  
+  private
+  
+    def create_message(params)
+      boot_twillio
+      
+      sms = @client.messages.create(
+        from: ENV["TWILIO_NUMBER"],
+        to: params("from_number"),
+        body: params("text")
+      )
+      render json: { state: 200 }
+    end
+    
+    def boot_twillio
+      account_sid = ENV["TWILIO_ACCOUNT_SID"]
+      auth_token = ENV["TWILIO_AUTH_TOKEN"]
+      @client = Twilio::REST::Client.new account_sid, auth_token
+    end
 end
