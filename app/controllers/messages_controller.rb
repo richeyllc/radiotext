7e321db2a8ba4f9a641798ead49f560ce56d4570
Teddy Smoker <pbadarau@gmail.com>
2017-07-12 16:08:21 +0000

    Add create action and strong params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,4 +9,19 @@ class MessagesController < ApplicationController
     @messages = Message.for_number(params[:id])
     @new_message = Message.new(number: params[:id])
   end
+  
+  def create
+    message = Message.new(message_params)
+    message.inbound = false
+    
+    if message.save
+      redirect_to "/messages/#{message.number}"
+    end
+  end
+  
+  private
+  
+    def message_params
+      params.require(:message).permit(:number, :text)
+    end
 end
