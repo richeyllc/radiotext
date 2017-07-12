58ad10d3a0e30dd0f6af769fd762405e3f2ebbb3
Teddy Smoker <pbadarau@gmail.com>
2017-07-12 15:31:35 +0000

    Generate messages model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170709114757) do
+ActiveRecord::Schema.define(version: 20170712153024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20170709114757) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["competition_id"], name: "index_competitors_on_competition_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "number"
+    t.string "text"
+    t.boolean "inbound"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
