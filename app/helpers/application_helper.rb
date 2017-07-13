59483fe6a48df2c2ffa33c493158be20489854db
Teddy Smoker <pbadarau@gmail.com>
2017-07-13 19:13:49 +0000

    Add method for replying to sms messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,28 @@
 module ApplicationHelper
+  def send_sms message
+    account_sid = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = ENV["TWILIO_AUTH_TOKEN"]
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    
+    @client.messages.create(
+      from: ENV["TWILIO_NUMBER"],
+      to: message.number,
+      body: message.text
+    )
+  end
+  
+  
   def send_cable message
     html = render_message(message)
     ActionCable.server.broadcast 'messages',
       number: message.number,
       html: html
   end
-  
+
   def render_message message
     ApplicationController.render({
       partial: 'messages/message',
       locals: { message: message }
     })
-  end
+end
 end
