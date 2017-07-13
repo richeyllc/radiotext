42116ee670999212d0ccb7ab1b4e543b832d6263
Teddy Smoker <pbadarau@gmail.com>
2017-07-13 19:07:32 +0000

    Add create.js

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,19 +1,18 @@
 App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
+
     var list      = $('.numbers');
     var thread    = $('.thread');
     var number    = thread.data('number');
     var latest    = $('.message[data-number="'+data.number+'"]');
-    
-    // prepend to message thread
-    if (thread.length && data.number == number) thread.prepend(data.html)
-    
-    // prepend to the list of ongoing threads
+
+    if (thread.length && data.number == number) thread.prepend(data.html);
+
     if (list.length) {
       latest.remove();
       list.prepend(data.html);
     }
-    
+
     $('.message:first').transition('flash');
   }
-})
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,28 +1,27 @@
 class MessagesController < ApplicationController
   include ApplicationHelper
-  
+
   def index
-    # @messages = Message.recent_by_number.by_date
-    @messages = Message.all.order("created_at DESC")
+     @messages = Message.all
   end
-  
+
   def show
     @messages = Message.for_number(params[:id])
     @new_message = Message.new(number: params[:id])
   end
-  
+
   def create
-    message = Message.new(message_params)
+    message = Message.new(clean_params)
     message.inbound = false
-    
+
     if message.save
       redirect_to "/messages/#{message.number}"
     end
   end
-  
+
   private
-  
-    def message_params
-      params.require(:message).permit(:number, :text)
-    end
+
+  def clean_params
+    params.require(:message).permit(:number, :text)
+  end
 end
