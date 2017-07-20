789f14b63a19ebf2bfbe40a4d6e06a3f0ace2037
Teddy Smoker <pbadarau@gmail.com>
2017-07-20 13:26:54 +0000

    Add create action with add_to_conversations and conversated? private methods

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,6 +2,28 @@ class ConversationsController < ApplicationController
   def index
     session[:conversations] ||= []
     
+    @listeners = Listener.all
     @conversations = Conversation.includes(:messages).find(session[:conversations])
   end
+  
+  def create
+    @conversation = Conversation.all..order('created_at DESC')
+    
+    add_to_conversations unless conversated?
+    
+    respond_to do |format|
+      format.js
+    end
+  end
+  
+  private
+    
+    def add_to_conversations
+      session[:conversations] ||= []
+      session[:conversations] << @conversation.id
+    end
+    
+    def conversated?
+      session[:conversations].include?(@conversation.id)
+    end
 end
