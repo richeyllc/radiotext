fea19cbc0f331c0188de23b0fe854b62c9120e2c
Teddy Smoker <pbadarau@gmail.com>
2017-07-20 10:38:41 +0000

    Add association for listener model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,4 @@
 class Conversation < ApplicationRecord
+  has_many :messages, dependent: :destroy
+  belongs_to :sender, foreign_key: :sender_id, class_name: Listener
 end
--- app/models/listener.rb
+++ app/models/listener.rb
@@ -1,2 +1,4 @@
 class Listener < ApplicationRecord
+  has_many :messages
+  has_many :conversations, foreign_key: :sender_id
 end
